7389e0689226305ec9e3b1e8eef3bced69612c6f
SolowinFXVI <julien.n.jacquet@protonmail.com>
2020-04-23 19:04:34 +0200

    switched to version 2.0 with added support for a modular config file containing all the datatypes and infos

--- src/DataSetGenerator.py
+++ src/DataSetGenerator.py
@@ -13,21 +13,6 @@ comma_str = ',\n'
 file = open('output.json', 'w')
 
 
-def get_luminosite(lower_range, upper_range):
-    luminosite = random.randint(lower_range, upper_range)
-    return luminosite
-
-
-def get_temperature(lower_range, upper_range):
-    temperature = random.randint(lower_range, upper_range)
-    return temperature
-
-
-def get_mouvement(lower_range, upper_range):
-    mouvement = random.randint(lower_range, upper_range)
-    return mouvement
-
-
 def record_begin(dataset):
     dataset = dataset + '{"Content": ['
     return dataset
@@ -43,157 +28,140 @@ def record_ending(dataset):
     return dataset
 
 
-def single_depressif_record(dataset, iteration):
+def get_value(lower_range, upper_range):
+    value = random.randint(lower_range, upper_range)
+    return value
+
+
+def single_record(dataset, iteration, record_type_name, record_type_index, nb_units, json_object):
     dataset += '{'
     dataset += record_number_str + str(iteration) + comma_str
     dataset += recorder_ID_str + str(iteration) + comma_str
-    dataset += expected_type_str + '"depressif"' + comma_str
+    dataset += expected_type_str + '"' + record_type_name + '"' + comma_str
     dataset += data_str + '{'
-    dataset += luminosite_str + str(get_luminosite(luminosite_depressif_lower,luminosite_depressif_upper)) + comma_str
-    dataset += temperature_str + str(get_temperature(temperature_depressif_lower, temperature_depressif_upper)) + comma_str
-    dataset += mouvement_str + str(get_mouvement(mouvement_depressif_lower, mouvement_depressif_upper))
+    for i in range(nb_units - 1):
+        dataset += '"' + json_object["dataTypes"][record_type_index]["dataUnits"][i]["unitType"] + '" : ' + str(
+            get_value(json_object["dataTypes"][record_type_index]["dataUnits"][i]["lowerLimit"],
+                      json_object["dataTypes"][record_type_index]["dataUnits"][i]["upperLimit"])) + comma_str
+    # sans comma pour le dernier
+    dataset += '"' + json_object["dataTypes"][record_type_index]["dataUnits"][nb_units - 1]["unitType"] + '" : ' + str(
+        get_value(json_object["dataTypes"][record_type_index]["dataUnits"][nb_units - 1]["lowerLimit"],
+                  json_object["dataTypes"][record_type_index]["dataUnits"][nb_units - 1]["upperLimit"]))
     dataset += '}}'
     return dataset
 
 
-def single_anxieux_record(dataset, iteration):
+def min_value(unit_type_index: int, json_object):
+    minimum = json_object["dataTypes"][0]["dataUnits"][unit_type_index]["lowerLimit"]
+    for j in range(json_object["numberOfTypes"]):
+        if json_object["dataTypes"][j]["dataUnits"][unit_type_index]["lowerLimit"] < minimum:
+            minimum = json_object["dataTypes"][j]["dataUnits"][unit_type_index]["lowerLimit"]
+    return minimum
+
+
+def max_value(unit_type_index: int, json_object):
+    maximum = json_object["dataTypes"][0]["dataUnits"][unit_type_index]["upperLimit"]
+    for j in range(json_object["numberOfTypes"]):
+        if json_object["dataTypes"][j]["dataUnits"][unit_type_index]["upperLimit"] > maximum:
+            maximum = json_object["dataTypes"][j]["dataUnits"][unit_type_index]["upperLimit"]
+    return maximum
+
+
+def alea_single_record(dataset, iteration, record_type_name, nb_units, json_object):
     dataset += '{'
     dataset += record_number_str + str(iteration) + comma_str
     dataset += recorder_ID_str + str(iteration) + comma_str
-    dataset += expected_type_str + '"anxieux"' + comma_str
+    dataset += expected_type_str + '"' + record_type_name + '"' + comma_str
     dataset += data_str + '{'
-    dataset += luminosite_str + str(get_luminosite(luminosite_anxieux_lower,luminosite_anxieux_upper)) + comma_str
-    dataset += temperature_str + str(get_temperature(temperature_anxieux_lower,temperature_anxieux_upper)) + comma_str
-    dataset += mouvement_str + str(get_mouvement(mouvement_anxieux_lower, mouvement_anxieux_upper))
+    for i in range(nb_units - 1):
+        dataset += '"' + json_object["dataTypes"][0]["dataUnits"][i]["unitType"] + '" : ' + str(
+            get_value(min_value(i, json_object), max_value(i, json_object))) + comma_str
+    # sans comma pour le dernier
+    dataset += '"' + json_object["dataTypes"][0]["dataUnits"][nb_units - 1]["unitType"] + '" : ' + str(
+        get_value(min_value(nb_units-1, json_object),
+                  max_value(nb_units-1, json_object)))
     dataset += '}}'
     return dataset
 
 
-def single_normal_record(dataset, iteration):
-    dataset += '{'
-    dataset += record_number_str + str(iteration) + comma_str
-    dataset += recorder_ID_str + str(iteration) + comma_str
-    dataset += expected_type_str + '"normal"' + comma_str
-    dataset += data_str + '{'
-    dataset += luminosite_str + str(get_luminosite(luminosite_normal_lower, luminosite_normal_upper)) + comma_str
-    dataset += temperature_str + str(get_temperature(temperature_normal_lower, temperature_normal_upper)) + comma_str
-    dataset += mouvement_str + str(get_mouvement(mouvement_normal_lower,mouvement_normal_upper))
-    dataset += '}}'
+def get_input_from_file(filepath):
+    fileInput = open(filepath, 'r')
+    fileContent = fileInput.read()
+    json_object = json.loads(fileContent)
+    fileInput.close()
+    return json_object
+
+
+def calculate_nb_alea(records, json_object):
+    #TODO check if percentages add up to 100
+    other_records = 0
+    for i in range(json_object["numberOfTypes"]):
+        other_records += records * (json_object["dataTypes"][i]["percent"] / 100)
+    print("number of other records : " + str(other_records))
+    return (records - other_records)
+
+
+def alea_records(dataset, json_object, iterations):
+    for a in range(int(tirages_alea)):
+        dataset = alea_single_record(dataset, iterations, "alea", json_object["numberOfUnits"], json_object)
+        dataset = record_comma(dataset)
+        iterations += 1
     return dataset
 
 
-def single_alea_record(dataset, iteration):
-    dataset += '{'
-    dataset += record_number_str + str(iteration) + comma_str
-    dataset += recorder_ID_str + str(iteration) + comma_str
-    dataset += expected_type_str + '"alea"' + comma_str
-    dataset += data_str + '{'
-    dataset += luminosite_str + str(get_luminosite(luminosite_alea_lower, luminosite_alea_upper)) + comma_str
-    dataset += temperature_str + str(get_temperature(temperature_alea_lower, temperature_alea_upper)) + comma_str
-    dataset += mouvement_str + str(get_mouvement(mouvement_alea_lower,mouvement_alea_upper))
-    dataset += '}}'
-    return dataset
-
-def mixer(dataset):
+def non_mixed(dataset, json_object):
     iterations = 1
-
-    for x in range(int(nb_anxieux)):
-        dataset = single_anxieux_record(dataset, iterations)
+    for i in range(json_object["numberOfTypes"] - 1):
+        for j in range(int(records * (json_object["dataTypes"][i]["percent"] / 100))):
+            dataset = single_record(dataset, iterations, json_object["dataTypes"][i]["TypeName"], i,
+                                    json_object["numberOfUnits"], json_object)
+            dataset = record_comma(dataset)
+            iterations += 1
+    for j in range(int(records * (json_object["dataTypes"][json_object["numberOfTypes"] - 1]["percent"] / 100))):
+        dataset = single_record(dataset, iterations,
+                                json_object["dataTypes"][json_object["numberOfTypes"] - 1]["TypeName"],
+                                json_object["numberOfTypes"] - 1, json_object["numberOfUnits"], json_object)
         dataset = record_comma(dataset)
         iterations += 1
-    # dataset = single_anxieux_record(dataset, iterations)
-    # iterations += 1
-
-    for x in range(int(nb_normaux)):
-        dataset = single_normal_record(dataset, iterations)
-        dataset = record_comma(dataset)
-        iterations += 1
-    #dataset = single_normal_record(dataset, iterations)
-    #iterations += 1
-
-    for x in range(int(nb_depressifs)):
-        dataset = single_depressif_record(dataset, iterations)
-        dataset = record_comma(dataset)
-        iterations += 1
-    #dataset = single_depressif_record(dataset, iterations)
-    # iterations += 1
-
-    for x in range(int(nb_alea)-1):
-        dataset = single_alea_record(dataset, iterations)
-        dataset = record_comma(dataset)
-        iterations += 1
-    dataset = single_alea_record(dataset, iterations)
-    iterations += 1
-
+    dataset = alea_records(dataset, json_object, iterations)
+    dataset = dataset[:-1]
     return dataset
 
 
+def mixed(dataset):
+    #TODO
+    print("not yet supported")
+    return dataset
+
+
+def processing(dataset, json_object):
+    if json_object["scramble"] == False:
+        dataset = non_mixed(dataset, json_object)
+    else:
+        dataset = mixed(dataset)
+    return dataset
 
 
 # MAIN
 print("START")
 
-#basics
+inputData = get_input_from_file("settings.json")
+
+# basics
 records = int(input("Nombre de records : "))
-print("Veuillez entrer les pourcentage des catégories de personnes, la partie restante des poucentages est aléatoire")
-depressifs = int(input("Pourcentage de depressifs : "))
-anxieux = int(input("pourcentage d'anxieux : "))
-normaux = int(input("pourcentage de normaux  : "))
-tirage_alea = 100 - (normaux + anxieux + depressifs)
-print("pourcentage aléatoire " + str(tirage_alea))
-nb_depressifs = records * (depressifs / 100)
-print(str(nb_depressifs) + " depressifs")
-nb_anxieux = records * (anxieux / 100)
-print(str(nb_anxieux) + " anxieux")
-nb_normaux = records * (normaux / 100)
-print(str(nb_normaux) + " normaux")
-nb_alea = records * (tirage_alea / 100)
-print(str(nb_alea) + " tirage aleatoire")
+tirages_alea: int = calculate_nb_alea(records, inputData)
+print(str(tirages_alea) + " tirage aleatoire")
 
-#ranges CHANGER LES VALEURS ICI
-luminosite_anxieux_lower = 100
-luminosite_depressif_lower = 100
-luminosite_normal_lower = 200
-
-luminosite_anxieux_upper = 300
-luminosite_depressif_upper = 300
-luminosite_normal_upper = 500
-
-temperature_anxieux_lower = 19
-temperature_depressif_lower = 16
-temperature_normal_lower = 19
-
-temperature_anxieux_upper = 24
-temperature_depressif_upper = 20
-temperature_normal_upper = 24
-
-mouvement_anxieux_lower = 6500
-mouvement_depressif_lower = 0
-mouvement_normal_lower = 5000
-
-mouvement_anxieux_upper = 12500
-mouvement_depressif_upper = 7500
-mouvement_normal_upper = 10000
-
-mouvement_alea_lower = min(mouvement_anxieux_lower, mouvement_depressif_lower, mouvement_normal_lower)
-mouvement_alea_upper = max(mouvement_anxieux_upper, mouvement_depressif_upper, mouvement_normal_upper)
-
-luminosite_alea_lower = min(luminosite_anxieux_lower, luminosite_depressif_lower, luminosite_normal_lower)
-luminosite_alea_upper = max(luminosite_anxieux_upper, luminosite_depressif_upper, luminosite_normal_upper)
-
-temperature_alea_lower = min(temperature_depressif_lower, temperature_anxieux_lower, temperature_normal_lower)
-temperature_alea_upper = max(temperature_normal_upper, temperature_anxieux_upper, temperature_depressif_upper)
-
-#core
+# core
 dataset = ""
 dataset = record_begin(dataset)
-dataset = mixer(dataset)
+dataset = processing(dataset, inputData)
 dataset = record_ending(dataset)
 
-
-#end
-#print(dataset)
+# end
+# print(dataset)
 res = json.loads(dataset)
 file.write(json.dumps(res, sort_keys=False, indent=4))
 file.close()
+
 print("END")
